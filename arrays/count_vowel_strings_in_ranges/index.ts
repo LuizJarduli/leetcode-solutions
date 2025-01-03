2e2fbcfce6a8239903390a0946f47fde6a55480e
LuizJarduli <luizjarduli@gmail.com>
2025-01-02 21:38:28 -0300

    feat: BenchmarkRunner utility class developed

--- arrays/count_vowel_strings_in_ranges/index.ts
+++ arrays/count_vowel_strings_in_ranges/index.ts
@@ -3,12 +3,8 @@ import case_2 from "./use_cases/case_2";
 import case_3 from "./use_cases/case_3";
 import case_4 from "./use_cases/case_4";
 import solution from "./solution";
+import { BenchmarkRunner } from "../../utils/BenchmarkRunner";
 
 const cases = [case_1, case_2, case_3, case_4];
 
-cases.forEach((args, index) => {
-  const benchMarkLabel = `\n\nbenchmark: use case ${index + 1}\n\n`;
-  console.time(benchMarkLabel);
-  console.log(`\nresult: ${solution(...args)}`);
-  console.timeEnd(benchMarkLabel);
-});
+new BenchmarkRunner(solution).run(cases);
